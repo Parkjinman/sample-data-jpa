568e755d092ae6c080b06497a894d4be12e41244
jin <snf01003@ex-em.com>
2023-08-02 14:33:03 +0900

    최초 커밋

---
+++ src/main/java/study/sampledatajpa/SampleDataJpaApplication.java
@@ -0,0 +1,13 @@
+package study.sampledatajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleDataJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/study/sampledatajpa/SampleDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package study.sampledatajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
