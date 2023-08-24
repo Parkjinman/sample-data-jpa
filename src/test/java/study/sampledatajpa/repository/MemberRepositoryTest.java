c1d2f2e825a0e3d6cbe646947d95bcd24ed3f348
jin <snf01003@ex-em.com>
2023-08-24 11:16:43 +0900

    벌크성 수정 쿼리 학습완료

--- src/test/java/study/sampledatajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/sampledatajpa/repository/MemberRepositoryTest.java
@@ -13,6 +13,8 @@ import study.sampledatajpa.dto.MemberDto;
 import study.sampledatajpa.entity.Member;
 import study.sampledatajpa.entity.Team;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.Arrays;
 import java.util.List;
 
@@ -24,8 +26,9 @@ import static org.assertj.core.api.Assertions.*;
 class MemberRepositoryTest {
 
     @Autowired MemberRepository memberRepository;
-    @Autowired
-    private TeamRepository teamRepository;
+    @Autowired TeamRepository teamRepository;
+    @PersistenceContext
+    EntityManager em;
 
     @Test
     public void testMember() {
@@ -187,6 +190,28 @@ class MemberRepositoryTest {
         assertThat(page.getTotalPages()).isEqualTo(2);
         assertThat(page.isFirst()).isTrue();
         assertThat(page.hasNext()).isTrue();
+    }
+
+    @Test
+    public void bulkUpdate() {
+        //given
+        memberRepository.save(new Member("member1", 10));
+        memberRepository.save(new Member("member2", 19));
+        memberRepository.save(new Member("member3", 20));
+        memberRepository.save(new Member("member4", 21));
+        memberRepository.save(new Member("member5", 40));
+
+        //when
+        int resultCount = memberRepository.bulkAgePlus(20);
+//        em.flush();
+//        em.clear();
+
+        List<Member> result = memberRepository.findByUsername("member5");
+        Member member5 = result.get(0);
+        System.out.println("member5 = " + member5);
+
+        //then
+        assertThat(resultCount).isEqualTo(3);
     }
 
 }
